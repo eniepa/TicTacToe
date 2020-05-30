d73b5cb11a878dc8420186638d4a9397d73ea1cb
eniepa <apeine.k@gmail.com>
2020-05-30 17:37:51 +0300

    17.37

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,16 +29,25 @@
         private void InitializeComponent()
         {
             this.Grid = new System.Windows.Forms.TableLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             this.Grid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
             // 
             // Grid
@@ -47,15 +56,15 @@
             this.Grid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.Grid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.Grid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.Grid.Controls.Add(this.label9, 2, 2);
-            this.Grid.Controls.Add(this.label8, 1, 2);
-            this.Grid.Controls.Add(this.label7, 0, 2);
-            this.Grid.Controls.Add(this.label6, 2, 1);
-            this.Grid.Controls.Add(this.label5, 1, 1);
-            this.Grid.Controls.Add(this.label4, 0, 1);
-            this.Grid.Controls.Add(this.label3, 2, 0);
-            this.Grid.Controls.Add(this.label2, 1, 0);
-            this.Grid.Controls.Add(this.label1, 0, 0);
+            this.Grid.Controls.Add(this.pictureBox9, 2, 2);
+            this.Grid.Controls.Add(this.pictureBox8, 1, 2);
+            this.Grid.Controls.Add(this.pictureBox7, 0, 2);
+            this.Grid.Controls.Add(this.pictureBox6, 2, 1);
+            this.Grid.Controls.Add(this.pictureBox5, 1, 1);
+            this.Grid.Controls.Add(this.pictureBox4, 0, 1);
+            this.Grid.Controls.Add(this.pictureBox3, 2, 0);
+            this.Grid.Controls.Add(this.pictureBox2, 1, 0);
+            this.Grid.Controls.Add(this.pictureBox1, 0, 0);
             this.Grid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Grid.Location = new System.Drawing.Point(0, 0);
             this.Grid.Name = "Grid";
@@ -66,122 +75,86 @@
             this.Grid.Size = new System.Drawing.Size(446, 383);
             this.Grid.TabIndex = 0;
             // 
-            // label1
+            // pictureBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 127);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "a";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 121);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
-            // label2
+            // pictureBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label2.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label2.Location = new System.Drawing.Point(151, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(142, 127);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "a";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label2.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(151, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(142, 121);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
-            // label3
+            // pictureBox3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label3.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label3.Location = new System.Drawing.Point(299, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(144, 127);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "a";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label3.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(299, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
             // 
-            // label4
+            // pictureBox4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label4.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label4.Location = new System.Drawing.Point(3, 127);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(142, 127);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "a";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label4.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox4.Location = new System.Drawing.Point(3, 130);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(142, 121);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
-            // label5
+            // pictureBox5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label5.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label5.Location = new System.Drawing.Point(151, 127);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(142, 127);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "a";
-            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label5.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox5.Location = new System.Drawing.Point(151, 130);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(142, 121);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
             // 
-            // label6
+            // pictureBox6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label6.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label6.Location = new System.Drawing.Point(299, 127);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(144, 127);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "a";
-            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label6.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox6.Location = new System.Drawing.Point(299, 130);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(144, 121);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
             // 
-            // label7
+            // pictureBox7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label7.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label7.Location = new System.Drawing.Point(3, 254);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(142, 129);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "a";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label7.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox7.Location = new System.Drawing.Point(3, 257);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(142, 123);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
             // 
-            // label8
+            // pictureBox8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label8.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label8.Location = new System.Drawing.Point(151, 254);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(142, 129);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "a";
-            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label8.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox8.Location = new System.Drawing.Point(151, 257);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(142, 123);
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
             // 
-            // label9
+            // pictureBox9
             // 
-            this.label9.AutoSize = true;
-            this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label9.Font = new System.Drawing.Font("MS PGothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label9.Location = new System.Drawing.Point(299, 254);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(144, 129);
-            this.label9.TabIndex = 8;
-            this.label9.Text = "a";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label9.Click += new System.EventHandler(this.Player_Click);
+            this.pictureBox9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox9.Location = new System.Drawing.Point(299, 257);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(144, 123);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
             // 
             // Form1
             // 
@@ -192,23 +165,30 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.Grid.ResumeLayout(false);
-            this.Grid.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
         private System.Windows.Forms.TableLayoutPanel Grid;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,7 @@ namespace TicTacToe
     public partial class Form1 : Form
     {
         bool xPlayerTurn = true;
+        int turnCount = 0;
 
         public Form1()
         {
@@ -27,54 +28,112 @@ namespace TicTacToe
             Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
         }
 
+        private void RestartGame()
+        {
+            InitializeCell();
+            turnCount = 0;
+        }
         private void InitializeCell()
         {
             string labelName;
             for (int i=1; i<=9; i++)
             {
-                labelName = "label" + i;
-                Grid.Controls[labelName].Text = string.Empty;
+                labelName = "pictureBox" + i;
+                Grid.Controls[labelName].Tag = string.Empty;
+                Grid.Controls[labelName].BackColor = Color.Transparent;
             }
         }
 
         private void Player_Click(object sender, EventArgs e)
         {
-            Label label = (Label)sender;
+            PictureBox pic = (PictureBox)sender;
 
-            if (label.Text != string.Empty)
+            if (pic.Tag != string.Empty)
             {
                 return;
             }
 
             if (xPlayerTurn)
             {
-                label.Text = "X";
+                pic.Tag = "X";
             }
             else
             {
-                label.Text = "O";
+                pic.Tag = "O";
             }
+            turnCount++;
+            PlaySound("click_sound");
             CheckForWin();
+            CheckForDraw();
             xPlayerTurn = !xPlayerTurn;
         }
 
         private void CheckForWin()
         {
             if (
-                    (label1.Text == label2.Text && label2.Text == label3.Text && label1.Text != string.Empty) ||
-                    (label4.Text == label5.Text && label5.Text == label6.Text && label4.Text != string.Empty) ||
-                    (label7.Text == label8.Text && label8.Text == label9.Text && label7.Text != string.Empty) ||
-                    (label1.Text == label4.Text && label4.Text == label7.Text && label1.Text != string.Empty) ||
-                    (label2.Text == label5.Text && label5.Text == label8.Text && label2.Text != string.Empty) ||
-                    (label3.Text == label6.Text && label6.Text == label9.Text && label3.Text != string.Empty) ||
-                    (label1.Text == label5.Text && label5.Text == label9.Text && label1.Text != string.Empty) ||
-                    (label3.Text == label5.Text && label5.Text == label7.Text && label3.Text != string.Empty)
+                    (pictureBox1.Tag == pictureBox2.Tag && pictureBox2.Tag == pictureBox3.Tag && pictureBox2.Tag != string.Empty) ||
+                    (pictureBox4.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox6.Tag && pictureBox4.Tag != string.Empty) ||
+                    (pictureBox7.Tag == pictureBox8.Tag && pictureBox8.Tag == pictureBox9.Tag && pictureBox7.Tag != string.Empty) ||
+                    (pictureBox1.Tag == pictureBox4.Tag && pictureBox4.Tag == pictureBox7.Tag && pictureBox1.Tag != string.Empty) ||
+                    (pictureBox2.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox8.Tag && pictureBox2.Tag != string.Empty) ||
+                    (pictureBox3.Tag == pictureBox6.Tag && pictureBox6.Tag == pictureBox9.Tag && pictureBox3.Tag != string.Empty) ||
+                    (pictureBox1.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox9.Tag && pictureBox1.Tag != string.Empty) ||
+                    (pictureBox3.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox7.Tag && pictureBox3.Tag != string.Empty)
                 )
             {
-
+                GameOver();
             }
         }
 
+        private void WinnerCellsChangeColor()
+        {
+            if (pictureBox1.Tag == pictureBox2.Tag && pictureBox1.Tag == pictureBox3.Tag && pictureBox1.Tag != "")
+            {
+                ChangeCellColors(pictureBox1, pictureBox2, pictureBox3, Color.Purple);
+            }
+            else if (pictureBox4.Tag == pictureBox5.Tag && pictureBox4.Tag == pictureBox6.Tag && pictureBox4.Tag != "")
+            {
+                ChangeCellColors(pictureBox4, pictureBox5, pictureBox6, Color.Purple);
+            }
+            else if (pictureBox7.Tag == pictureBox8.Tag && pictureBox7.Tag == pictureBox9.Tag && pictureBox7.Tag != "")
+            {
+                ChangeCellColors(pictureBox7, pictureBox8, pictureBox9, Color.Purple);
+            }
+            else if (pictureBox1.Tag == pictureBox4.Tag && pictureBox1.Tag == pictureBox7.Tag && pictureBox1.Tag != "")
+            {
+                ChangeCellColors(pictureBox1, pictureBox4, pictureBox7, Color.Purple);
+            }
+            else if (pictureBox2.Tag == pictureBox5.Tag && pictureBox2.Tag == pictureBox8.Tag && pictureBox2.Tag != "")
+            {
+                ChangeCellColors(pictureBox2, pictureBox5, pictureBox8, Color.Purple);
+            }
+            else if (pictureBox3.Tag == pictureBox6.Tag && pictureBox3.Tag == pictureBox9.Tag && pictureBox3.Tag != "")
+            {
+                ChangeCellColors(pictureBox3, pictureBox6, pictureBox9, Color.Purple);
+            }
+            else if (pictureBox1.Tag == pictureBox5.Tag && pictureBox1.Tag == pictureBox9.Tag && pictureBox1.Tag != "")
+            {
+                ChangeCellColors(pictureBox1, pictureBox5, pictureBox9, Color.Purple);
+            }
+            else if (pictureBox3.Tag == pictureBox5.Tag && pictureBox3.Tag == pictureBox7.Tag && pictureBox3.Tag != "")
+            {
+                ChangeCellColors(pictureBox3, pictureBox5, pictureBox7, Color.Purple);
+            }
+
+        }
+        private void ChangeCellColors(PictureBox firstLabel, PictureBox secondLabel, PictureBox thirdLabel, Color color)
+        {
+            firstLabel.BackColor = color;
+            secondLabel.BackColor = color;
+            thirdLabel.BackColor = color;
+        }
+
+        private void PlaySound(string soundName)
+        {
+            System.IO.Stream str = (System.IO.Stream)Properties.Resources.ResourceManager.GetObject(soundName);
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+            snd.Play();
+        }
         private void GameOver()
         {
             string winner;
@@ -86,7 +145,18 @@ namespace TicTacToe
             {
                 winner = "O";
             }
-            MessageBox.Show(winner + " wins!");
+            WinnerCellsChangeColor();
+           MessageBox.Show(winner + " wins!");
+            RestartGame();
+        }
+
+        private void CheckForDraw()
+        {
+            if (turnCount == 9)
+            {
+                MessageBox.Show("Draw!");
+                RestartGame();
+            }
         }
     }
 }
